2b3f33b5a58d18ba251c605fe5615bf32bb77ab0
Redemona Christ <redemonachrist@gmail.com>
2024-12-19 17:50:23 +0100

    Premier commit

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  reactStrictMode: true,
+
+//   images: {
+//     domains: ['cdnjs.cloudflare.com'],
+// },
+  
+};
